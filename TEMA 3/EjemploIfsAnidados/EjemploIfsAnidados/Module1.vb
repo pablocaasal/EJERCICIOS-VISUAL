edb8cda110866d5e1e36dd8aba1d351ca11a0ed1
pablocaasal <pablo.casal.romero@ciclosmontecastelo.com>
2022-10-10 13:33:17 +0200

    boletin3

---
+++ TEMA 3/EjemploIfsAnidados/EjemploIfsAnidados/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numero As Integer
+        Dim continuar As Char
+        Do
+            Console.WriteLine("Introduce un número.")
+            numero = Convert.ToInt32(Console.ReadLine())
+
+            If numero Mod 6 = 0 Then
+                    If numero > 100 Then
+                        Console.WriteLine("Múltiplo de 6 y > 100")
+                    Else
+                        Console.WriteLine("Múltiplo de 6 y <= 100")
+
+                    End If
+                Else
+                    If numero > 100 Then
+                        Console.WriteLine("No multiplo de 6 y > 100")
+                    Else
+                        Console.WriteLine("No multiplo de 6 y =< 100")
+                    End If
+                End If
+                Console.WriteLine("Quiere continuar en el programa? s/n")
+                continuar = Console.ReadLine
+        Loop While continuar = "s"c
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ TEMA 3/EjemploIfsAnidados/EjemploIfsAnidados/Module2.vb
@@ -0,0 +1,16 @@
+﻿Module Module2
+    Sub main()
+        Dim numero As Integer
+        Dim c1 As Integer
+        Do
+            If numero Mod 6 And numero > 100 Then
+                Console.WriteLine("Felicidades el número es muúltiplo de 6 y mayor que 100")
+            ElseIf c1 += 3 Then
+            End If
+
+
+        Loop Until (numero Mod 6 And numero > 100) Or c1 = 3
+
+
+    End Sub
+End Module
